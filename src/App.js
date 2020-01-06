f72b282e5356e36773ecaa5279dc01e0d3632fdc
Johnny <jho037@ucr.edu>
2020-01-05 16:04:46 -0800

    Adding whole project

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ class App extends Component {
 
   onButtonSubmit = () =>{
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://gentle-crag-58821.herokuapp.com/imageurl', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -100,7 +100,7 @@ class App extends Component {
 
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://gentle-crag-58821.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
